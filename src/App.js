18963f0c505029358d74ed6aa7bf13567836d62d
pettersand <petter.sand@gmail.com>
2023-10-15 17:49:12 +0200

    basic components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  background-color: #333;
+  min-height: 100vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainContainer>
+        <Header />
+      </MainContainer>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div``;
+
+const Header = () => {
+  return <HeaderContainer></HeaderContainer>;
+};
+
+export default Header;
