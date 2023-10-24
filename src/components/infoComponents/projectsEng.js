e9360a2531f0623aff98bff3df2690933c02b9cb
pettersand <petter.sand@gmail.com>
2023-10-24 15:47:33 +0200

    styling & layout of projects

--- src/components/infoComponents/projectsEng.js
+++ src/components/infoComponents/projectsEng.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { useAppState } from "../../App";
 import styled from "styled-components";
+import {
+  SiDjango,
+  SiDocker,
+  SiGithub,
+  SiGithubactions,
+  SiPython,
+  SiSvelte,
+  SiTypescript,
+} from "react-icons/si";
 
 const ProjectWrapper = styled.div`
   display: flex;
@@ -30,9 +39,15 @@ const ProjectContainer = styled(({ isVisible, ...props }) => (
   flex: 1;
   height: 100%;
   padding: 16px;
-  gap: 4px;
+  gap: 16px;
   p {
     font-size: 0.9em;
+    margin: 0;
+    color: var(--paragraph);
+  }
+
+  h4 {
+    color: var(--headline);
   }
 `;
 
@@ -53,21 +68,77 @@ const ProjectHeader = styled.div`
   border-bottom: 1px solid var(--accent);
 `;
 
+const DetailsBar = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
 const ProjectItem = styled.div`
   width: 100%;
   flex: 1;
   display: flex;
   flex-direction: row;
-  gap: 4px;
+  gap: 8px;
 `;
 
-const TechStack = styled.div``;
+const ProjectStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  h4 {
+    margin: 0;
+  }
+`;
 
-const Features = styled.div``;
+const TechStack = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 16px;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const TechColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const TechIcon = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.5em;
+  margin-right: 4px;
+  color: var(--accent);
+  gap: 8px;
+  span {
+    font-size: 0.6em;
+    color: var(--paragraph);
+    margin-right: 8px;
+  }
+`;
+
+const Features = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  h5 {
+    color: var(--headline);
+    margin: 0;
+  }
+  span {
+    color: var(--paragraph);
+    font-size: 0.9em;
+  }
+`;
 
 const ImageContainer = styled.div`
   background-image: url(${(props) => props.bgImage});
-  background-size: fill;
+  background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   height: 80%;
@@ -84,16 +155,74 @@ const ProjectsEng = () => {
       <ProjectRow>
         <ProjectContainer isVisible={detailLevel === "minimal"}>
           <ProjectHeader>Builder</ProjectHeader>
+          <DetailsBar>
+            <span>26/08 -</span>
+            <span>
+              GitHub
+              <SiGithub />
+            </span>
+            <span>Link</span>
+          </DetailsBar>
           <ProjectItem>
-            <p>
-              Streamlines the creation and management of personalized workout
-              programs. The "one-stop-shop" app I wish I had while working in
-              the fitness industry.
-            </p>
-            <ImageContainer bgImage="/ExercisePlanner.png" />
+            <ProjectStack>
+              <p>
+                Streamlines the creation and management of personalized workout
+                programs. The "one-stop-shop" app I wish I had while working in
+                the fitness industry.
+              </p>
+              <h4>Tech Stack:</h4>
+              <TechStack>
+                <TechColumn>
+                  <TechIcon>
+                    <SiPython />
+                    <span>Python</span>
+                  </TechIcon>
+
+                  <TechIcon>
+                    <SiDjango />
+                    <span>Django</span>
+                  </TechIcon>
+                  <TechIcon>
+                    <SiSvelte />
+                    <span>Svelte</span>
+                  </TechIcon>
+                </TechColumn>
+                <TechColumn>
+                  <TechIcon>
+                    <SiTypescript />
+                    <span>TypeScript</span>
+                  </TechIcon>
+                  <TechIcon>
+                    <SiDocker />
+                    <span>Docker</span>
+                  </TechIcon>
+                  <TechIcon>
+                    <SiGithubactions />
+                    <span>GitHub Actions</span>
+                  </TechIcon>
+                </TechColumn>
+              </TechStack>
+            </ProjectStack>
+
+            <ImageContainer bgImage="/BuilderLogo.png" />
           </ProjectItem>
-          <ProjectItem>Tech Stack</ProjectItem>
-          <ProjectItem>Features</ProjectItem>
+          <ProjectItem>
+            <ProjectStack>
+              <h4>Features:</h4>
+              <Features>
+                <h5>Write Exercise Programs:</h5>
+                <span>
+                  Create advanced programs for yourself or your clients
+                </span>
+              </Features>
+              <Features>
+                <h5>Write Exercise Programs:</h5>
+                <span>
+                  Create advanced programs for yourself or your clients
+                </span>
+              </Features>
+            </ProjectStack>
+          </ProjectItem>
         </ProjectContainer>
         <ProjectContainer isVisible={detailLevel === "detailed"}>
           <ProjectHeader>Builder</ProjectHeader>
