9b7fd988560cfd5b21587728844174b400796b58
pettersand <petter.sand@gmail.com>
2023-10-26 12:26:34 +0200

    added AboutMe data

--- src/components/infoComponents/aboutMeEng.js
+++ src/components/infoComponents/aboutMeEng.js
@@ -4,43 +4,153 @@ import styled from "styled-components";
 
 const AboutMeWrapper = styled.div`
   display: flex;
+  flex-flow: column wrap;
   width: 100%;
   padding: 10px;
   align-items: center;
-  justify-content: center;
-  gap: 5px;
+  justify-content: flex-start;
+  gap: 24px;
+  padding: 24px;
 `;
 
-const AboutMeContainer = styled(({ isVisible, ...props }) => (
-  <div {...props} style={{ display: isVisible ? "flex" : "none" }} />
-))`
-  flex-direction: column;
+const TopRow = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  width: 100%;
+  gap: 16px;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-flow: column wrap;
   align-items: center;
   justify-content: flex-start;
+  width: 45%;
+`;
+
+const Divider = styled.div`
+  height: 80%;
+  border: 1px solid var(--bg);
+  margin-inline: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BottomRow = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+`;
+
+const ExtraContent = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  gap: 16px;
+`;
+
+const ProjectHeader = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.2em;
+  font-weight: 600;
+  padding: 4px;
+  border-bottom: 1px solid var(--accent);
+`;
+
+const TopContent = styled.div`
+  display: flex;
+  width: 90%;
+  align-items: center;
+  justify-content: flex-start;
+
+  p {
+    color: var(--paragraph);
+  }
+`;
+
+const BottomContent = styled.div`
+  display: flex;
+  width: 90%;
+  align-items: flex-start;
+  justify-content: center;
+
+  p {
+    color: var(--paragraph);
+  }
+`;
+
+const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 1;
-  height: 100%;
-  box-shadow: -10px 10px 8px rgba(0, 0, 0, 0.75);
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 const AboutMeEng = () => {
-  const { state } = useAppState();
-  const { detailLevel } = state;
-
   return (
     <AboutMeWrapper>
-      <AboutMeContainer isVisible={detailLevel === "minimal"}>
-        <h4>Changing Careers</h4>
-      </AboutMeContainer>
-      <AboutMeContainer isVisible={detailLevel === "detailed"}>
-        <h4>Changing Careers</h4>
-      </AboutMeContainer>
-
-      <AboutMeContainer isVisible={detailLevel === "minimal"}>
-        <h4>Learning Process</h4>
-      </AboutMeContainer>
-      <AboutMeContainer isVisible={detailLevel === "detailed"}>
-        <h4>Learning Process</h4>
-      </AboutMeContainer>
+      <TopRow>
+        <MainContent>
+          <ProjectHeader>About Me</ProjectHeader>
+          <TopContent>
+            <p>Yayuh, this is the first paragraph for the about me section.</p>
+          </TopContent>
+          <TopContent>
+            <p>Second paragraph goes here</p>
+          </TopContent>
+          <TopContent>
+            <p>Third row here</p>
+          </TopContent>
+        </MainContent>
+        <Divider />
+        <MainContent>
+          <ProjectHeader>Changing Careers</ProjectHeader>
+          <TopContent>
+            <p>Yayuh, this is the first paragraph for changing careers.</p>
+          </TopContent>
+          <TopContent>
+            <p>Second paragraph goes here</p>
+          </TopContent>
+          <TopContent>
+            <p>Third row here</p>
+          </TopContent>
+        </MainContent>
+      </TopRow>
+      <BottomRow>
+        <ExtraContent>
+          <ProjectHeader>My Learning Process</ProjectHeader>
+          <TopContent>
+            <ContentBox>Step by Step</ContentBox>
+            <ContentBox>Focus & Thought Process</ContentBox>
+            <ContentBox>Approach</ContentBox>
+          </TopContent>
+        </ExtraContent>
+        <ExtraContent>
+          <ProjectHeader>Passions & Motivations</ProjectHeader>
+          <BottomContent>
+            <ContentBox>Innovation</ContentBox>
+            <ContentBox>Creating</ContentBox>
+            <ContentBox>
+              Other Interests & Hobbies <p>Music</p>
+              <p>Gaming</p>
+              <p>Financial Markets</p>
+            </ContentBox>
+          </BottomContent>
+        </ExtraContent>
+      </BottomRow>
     </AboutMeWrapper>
   );
 };
