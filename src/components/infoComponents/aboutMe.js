e0a4cdfb759fc7cc06ae064c256d9cffa3fc9e55
pettersand <petter.sand@gmail.com>
2023-11-06 21:55:07 +0100

    further rework of aboutMe

--- src/components/infoComponents/aboutMe.js
+++ src/components/infoComponents/aboutMe.js
@@ -123,26 +123,28 @@ const AboutMe = () => {
             <p>
               I'm driven by a natural curiosity to understand the world around
               me, always asking why and how. This thirst for knowledge drives my
-              work, encouraging constant innovation and a real impact on
+              work, encouraging constant innovation and having a real impact on
               people's lives. I'm particularly drawn to technology's power to
-              address real-world challenges and create a brighter future. My
+              address real-world challenges and creating a brighter future. My
               enthusiasm stays high, whether I'm deep in code or dissecting data
               sets.
             </p>
             <p>
               Software development is my current focus, but I'm also open to the
               vast possibilities within the tech landscape. I'm excited about
-              roles that let me leverage my technical abilities to make a
-              tangible difference. My experiences as a personal trainer and
-              teaching assistant have sharpened my ability to connect with
-              others and tailor my approach to effectively meet their needs.
+              roles that would further fuel my knowledge in a setting with
+              likeminded individuals. My experiences as a personal trainer and
+              teaching assistant have sharpened my ability to connect with other
+              people on a deep level. Furthermore, it has taught me how to adapt
+              my approach to effectively understand their needs and preferences.
             </p>
             <p>
-              With a foundation in data analysis, I value evidence-based
-              decision-making and can blend insightful data interpretation with
-              understanding client needs to find optimal solutions. I look
-              forward to challenges that allow me to combine my technical
-              expertise with my passion for positive change.
+              With an academic foundation in data analysis, I value
+              evidence-based decision-making and understand the nuances of
+              blending insightful data interpretation with client needs in order
+              to find optimal solutions for both parties. I look forward to any
+              challenges that allow me to combine my technical expertise with my
+              passion for positive change.
             </p>
           </TopContent>
         </MainContent>
@@ -158,26 +160,29 @@ const AboutMe = () => {
               After thirteen years in the fitness industry, I recognized it was
               time for a change. Certain aspects no longer matched my goals,
               even though I gained valuable experience and insight into human
-              behavior. Technology and development appealed to me as they
-              matched my strengths and satisfied my desire for creativity,
-              challenge, and continuous learning.
+              behavior and psychology. Technology and development appealed to me
+              as they matched my personal strengths and satisfied my desire for
+              creativity, challenge, and continuous learning. I have always
+              enjoyed puzzles and finding new ways to solve old problems.
             </p>
           </TopContent>
           <TopContent>
             <p>
               I considered a formal education, but the urgency to move forward
-              and my responsibilities guided me towards self-study. Learning to
-              code during my free time turned into an engaging challenge, and it
-              didn't take long to confirm I had made the right choice.
+              and my financial and personal responsibilities guided me towards
+              self-study instead. Learning to code during my free time turned
+              into an engaging challenge, and it didn't take long to confirm
+              that I had made the right choice!
             </p>
           </TopContent>
           <TopContent>
             <p>
               Now, I've committed to development full-time. Juggling a job and
               learning was holding me back from diving into the IT field. By
-              focusing solely on this new career path, I can dedicate the
-              necessary time to excel. I'm ready to tackle this industry with
-              vigor and prove my abilities in this new and exciting field.
+              focusing solely on this new career path, I have been able to
+              dedicate the necessary time to boost my learning. I'm ready to
+              tackle this industry with vigor and prove my abilities in this new
+              and exciting field.
             </p>
           </TopContent>
         </MainContent>
@@ -190,39 +195,44 @@ const AboutMe = () => {
           </ProjectHeader>
           <BottomContent>
             <ContentBox>
-              <h4>Self-Directed Growth</h4>
               <p>
                 I believe in steering my own course, with growth being a
                 deliberate result of my efforts and vision. By focusing on areas
-                I can influence, I've turned knowledge gaps into expertise,
-                shaping my journey through each challenge and project with
-                determination and flexibility.
+                I can influence, I've turned knowledge gaps into bridges,
+                improving my understanding with each challenge and project. I am
+                well aware of what I have yet to learn, though it would be a
+                rather dull journey if I was already done.
               </p>
-              <h4>Beginning Steps</h4>
+              <h4>Beginning Steps: Courses</h4>
               <p>
                 You cannot build a house without a foundation. My learning
                 process started with thorough research, seeking insight into the
                 best foundational programming language, and how to learn it.
                 This led me to the course "Python for Everybody" on Coursera,
-                followed by Harvard's CS50x. However, I knew practical
+                closely followed by Harvard's CS50x. However, I knew practical
                 application was key to truly understanding the craft.
               </p>
-              <h4>Hands-On Experience</h4>
+              <h4>Hands-On Experience: Salloc</h4>
 
               <p>
-                My initial project, Salloc, transformed an Excel concept into a
-                practical web application, marking my entry into full-stack
-                development. Following that, building my own digital portfolio
-                introduced me to server management with Linux, Ubuntu, and Nginx
-                on a Hostinger VPS.
+                My initial project, Salloc, transformed an old Excel concept
+                into a practical web application, marking my entry into
+                full-stack development. Recognizing the need for a digital
+                portfolio, I designed a personal site to house my projects. This
+                initiative led me to explore the domains of hosting, diving into
+                the workings of Linux, Ubuntu, and Nginx with a VPS on
+                Hostinger.
               </p>
-              <h4>Innovations & Challenges</h4>
+              <h4>Innovations & Challenges: Builder</h4>
               <p>
-                The transition to Builder, a management tool for personal
-                trainers, was a natural evolution. It combined my fitness
-                background with my coding skills. I delved into DevOps, focusing
-                on clean code and reliability, ensuring this passion project
-                meets high standards over time.
+                The transition to Builder, my brain-child, a management tool for
+                personal trainers, was a natural evolution. It served as a
+                bridge between my fitness background and programming. This
+                project was also based on an Excel spreadsheet I actively used
+                for creating effective training programs for my clients.
+                Prioritizing clean code, security, and robust testing, I've
+                ventured into DevOps, utilizing containers and CI/CD pipelines,
+                ensuring this passion project meets high standards over time.
               </p>
             </ContentBox>
             <ContentBox>
@@ -235,11 +245,11 @@ const AboutMe = () => {
                 my commitment to efficiency, organization, and quality in the
                 software development process.
               </p>
-              <h4>Standing Out</h4>
+              <h4>Standing Out: Interactive Resume</h4>
               <p>
                 The challenge of pivoting careers in a turbulent economic
                 climate made it clear that I had to stand out from the crowd.
-                This interactive resume, built in React, is the response. This
+                This interactive resume, built in React, is my response. This
                 project reflects my proactive approach to obstacles, always
                 seeking unique solutions while expanding my tech repertoire.
               </p>
@@ -250,9 +260,9 @@ const AboutMe = () => {
                 balanced approach with curiosity and care. Being mindful of the
                 dangers of becoming a copy paste developer, I focused on using
                 ChatGPT as a "pocket senior developer"; meaning I used it as a
-                guide in tackling new concepts, and a mentor for brainstorming.
-                I believe it has been instrumental in allowing me to quickly
-                ipskill my programming.
+                guide for understanding new concepts, and a mentor for
+                brainstorming. I believe it has been instrumental in allowing me
+                to quickly upskill my programming.
               </p>
             </ContentBox>
           </BottomContent>
